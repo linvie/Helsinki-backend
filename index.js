8e17ddfd593231658655240ad2eb2d221c6606e4
linvie <Li_Ching_@outlook.com>
2023-07-28 23:34:09 +0800

    28 jul

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('content', function (req, res) { return JSON.stringify(req.body) })
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 app.use(cors())
+app.use(express.static('build'))
 
 
 let persons = [
